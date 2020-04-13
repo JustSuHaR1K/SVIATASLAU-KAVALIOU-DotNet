7e756c7ec159a3b3c367048526eba56654754289
JustSuHaR1K <57293362+JustSuHaR1K@users.noreply.github.com>
2020-04-13 23:42:52 +0300

    reworked

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CommandLine;
+using Lab1.CommandLineArguments;
+using Lab1.Models;
+using Lab1.Services;
+//using System;
+using System.Collections.Generic;
 
 namespace Lab1
 {
@@ -6,7 +11,30 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            
+            List<Student> students = new List<Student>();
+            string inputFile = string.Empty;
+            string outputFile = string.Empty;
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed(o =>
+                   {
+                       inputFile = o.InputFile;
+                       outputFile = o.OutputFile;
+                       if (!string.IsNullOrEmpty(inputFile))
+                       {
+                           CSVReader reader = new CSVReader();
+                           students = reader.Read(inputFile);
+                       }
+                       if (o.FileType.ToUpper() == "JSON")
+                       {
+                           JsonWriter jsonWriter = new JsonWriter();
+                           jsonWriter.Write(students, $"{outputFile}.json");
+                       }
+                       else if (o.FileType.ToUpper() == "EXCEL" || o.FileType.ToLower() == "xls")
+                       {
+                           ExcelWriter excelWriter = new ExcelWriter();
+                           excelWriter.Write(students, $"{outputFile}.xls");
+                       }
+                   });
         }
     }
 }
